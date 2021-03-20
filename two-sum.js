8bef8ea62a966745f313e67c6388f0f3caed44a8
nickjang <nickjang@alumni.upenn.edu>
2021-03-20 13:27:48 -0400

    Remove ESlint error

--- two-sum.js
+++ two-sum.js
@@ -3,6 +3,7 @@ let twoSum = function (nums, target) {
 
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
+    // eslint-disable-next-line eqeqeq
     if (frequency[target - num] != null) {
       return [i, frequency[target - num]];
     }
